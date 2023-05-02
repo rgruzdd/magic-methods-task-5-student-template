e5e026a8316d8be901df01983dfe8ef70bb19021
Gruzin <gruzd.roman@inbox.ru>
2023-05-02 18:06:02 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,6 +1,22 @@
 import os
 import shutil
+import tempfile
+from pathlib import Path
 
-class TempDir:
-    pass
-    # TODO: please ad your code here
+
+class TempDir(object):
+
+    def __init__(self, suffix=None, prefix=None, dir=None):
+        os.chdir("D:\\pythonProject63")
+        dir = os.getcwd()
+        self.name = tempfile.mkdtemp(suffix, prefix, dir=dir)
+        name = self.name.split('\\')[-1]
+        path = Path(self.name)
+        os.mkdir(self.name)
+
+    def __enter__(self):
+        return self.name
+
+    def __exit__(self, exc, value, tb):
+        shutil.rmtree(self.name)
+
