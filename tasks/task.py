08adbf329ad44771cc5883076cbd49b86094e4ea
Gruzin <gruzd.roman@inbox.ru>
2023-05-02 18:13:03 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -7,11 +7,8 @@ from pathlib import Path
 class TempDir(object):
 
     def __init__(self, suffix=None, prefix=None, dir=None):
-        os.chdir("D:\\pythonProject63")
         dir = os.getcwd()
         self.name = tempfile.mkdtemp(suffix, prefix, dir=dir)
-        name = self.name.split('\\')[-1]
-        path = Path(self.name)
         os.mkdir(self.name)
 
     def __enter__(self):
@@ -19,4 +16,3 @@ class TempDir(object):
 
     def __exit__(self, exc, value, tb):
         shutil.rmtree(self.name)
-
